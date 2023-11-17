b1a40822da5747a1c62bacb7fb9658561f9d3172
Matt Rudge <matt.rudge@codeinstitute.net>
2023-11-17 09:59:15 +0000

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 
-from .models import Whiskey
 from .forms import EditForm
+from .models import Whiskey
 
 # Create your views here.
 
@@ -13,14 +13,16 @@ class WhiskeyList(generic.ListView):
     template_name = "home/index.html"
 
 
-def EditWhiskey(request, id):
+def EditWhiskey(request, kieran):
 
     queryset = Whiskey.objects.all()
-    whiskey = get_object_or_404(queryset, id=id)
+    whiskey = get_object_or_404(queryset, id=kieran)
+
     if request.method == "POST":
         edit_form = EditForm(data=request.POST, instance=whiskey)
         edit_form.save()
         return HttpResponseRedirect(reverse('home'))
-    else:
-        edit_form = EditForm({'notes': whiskey.notes, 'rating': whiskey.rating})
-        return render(request, "home/edit.html", { "whiskey": whiskey, "edit_form": edit_form })
+
+    edit_form = EditForm({"notes": whiskey.notes, "rating": whiskey.rating})
+    return render(request, "home/edit.html", {"whiskey": whiskey, "edit_form": edit_form})
+
